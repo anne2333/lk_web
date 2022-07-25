91fe9e090cf9ca5f437447fff5b680f1cfb9e630
shijintao <710780244@qq.com>
2022-07-25 22:47:26 +0800

    导航及全屏下拉

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,26 @@
 $(function () {
+    new fullpage('#fullpage', {
+        //options here
+        autoScrolling: true,
+        scrollHorizontally: true
+    })
+
+
+    $('.nav-btn').click(function () {
+        $(this).toggleClass('nav-btn-active')
+        $('.bs-collapse').slideToggle(300)
+        // if ($(this).hasClass('nav-btn-active')) {
+        //     $(this).removeClass('nav-btn-active')
+        //     $(".head-active .logo").removeClass('showLogo');
+        //     $(".head-active").removeClass("showWite");
+        //     $('.head-mc').slideUp();
+        // } else {
+        //     $(this).addClass('nav-btn-active');
+        //     $('.head-mc').slideDown();
+        //     $(".head-active .logo").addClass('showLogo');
+        //     $(".head-active").addClass("showWite");
+        //     $('.head-active .logo img').attr('src', 'https://www.yuwell.com/web/bocweb/web/img2/logo-2.png?v=v1.2')
+        // }
+    })
+
 })
