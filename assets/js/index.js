69fb980256a8dbf1467dd3211a6486b0f002aa5e
shijintao <710780244@qq.com>
2022-07-26 16:44:51 +0800

    首页第一屏第二屏完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,20 @@
 $(function () {
-    new fullpage('#fullpage', {
-        //options here
-        autoScrolling: true,
-        scrollHorizontally: true
-    })
-
 
     $('.nav-btn').click(function () {
         $(this).toggleClass('nav-btn-active')
         $('.bs-collapse').slideToggle(300)
-        // if ($(this).hasClass('nav-btn-active')) {
-        //     $(this).removeClass('nav-btn-active')
-        //     $(".head-active .logo").removeClass('showLogo');
-        //     $(".head-active").removeClass("showWite");
-        //     $('.head-mc').slideUp();
-        // } else {
-        //     $(this).addClass('nav-btn-active');
-        //     $('.head-mc').slideDown();
-        //     $(".head-active .logo").addClass('showLogo');
-        //     $(".head-active").addClass("showWite");
-        //     $('.head-active .logo img').attr('src', 'https://www.yuwell.com/web/bocweb/web/img2/logo-2.png?v=v1.2')
-        // }
     })
 
+    $('.a_title').on('mouseenter', function () {
+        var src = $(this)[0].children[0].src
+        src = src.substr(0, src.length - 4)
+        console.log(src);
+        $(this)[0].children[0].src = src + '1.png'
+    })
+    $('.a_title').on('mouseleave', function () {
+        var src = $(this)[0].children[0].src
+        src = src.substr(0, src.length - 5)
+        console.log(src);
+        $(this)[0].children[0].src = src + '.png'
+    })
 })
